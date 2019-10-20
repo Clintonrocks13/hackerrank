fe549d6d58183223b107681dcb4bdcfe013bd3e9
Clinton Fernandes <clinfernandes13@gmail.com>
2019-10-20 16:14:47 +0530

    Update longest_substring.cpp

--- longest_substring.cpp
+++ longest_substring.cpp
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
+//this program can be optimised more but this solution is written so that it is understandable
 
 #include <vector>
 #include<algorithm>
